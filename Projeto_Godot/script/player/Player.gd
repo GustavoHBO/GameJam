39b585a1c400fae5dd688252365b1e3f1020e17e
Aldrey <marcusaldrey@gmail.com>
2019-04-06 08:26:30 -0300

    transformação

--- Projeto_Godot/script/player/Player.gd
+++ Projeto_Godot/script/player/Player.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 var motion = Vector2()
 var velocidade = 200
 
+var defaultTexture = preload("res://icon.png")
+var transformationTexture = preload("res://key.png")
+var isTransformed = false
+
 var pos_x_inicial
 var pos_y_inicial
 
@@ -22,6 +26,13 @@ func _physics_process(delta):
 		motion.x = velocidade
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -velocidade
+	elif Input.is_action_just_pressed("ui_select"):
+		if isTransformed:
+			get_node("Sprite").set_texture(defaultTexture)
+			isTransformed = false
+		else:
+			get_node("Sprite").set_texture(transformationTexture)
+			isTransformed = true;
 	
 	move_and_slide(motion)
 
