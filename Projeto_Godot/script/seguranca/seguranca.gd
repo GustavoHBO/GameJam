5d969ba01a76afa854a8073254adfc964d018e63
Gustavo Henrique <ghboliveira@hotmail.com>
2019-04-06 01:27:40 -0300

    Movimentação horizontal e vertical

--- Projeto_Godot/script/seguranca/seguranca.gd
+++ Projeto_Godot/script/seguranca/seguranca.gd
@@ -1,20 +1,25 @@
 extends StaticBody2D
 
 var pos_inicial
-var pos_esquerda
-var pos_direita
+var pos_1
+var pos_2
 
-var dist_esquerda = 100
-var dist_direita = 50
+var orientacao = true # true para horizontal, false para vertical
+
+var dist_1 = 10
+var dist_2 = 10
 
 var direcao = 0 # direção 0 faz com que o lado da movimentação seja sorteado, 1 move para direita, 2 para a esquerda
 
 var velocidade = 1
 
 func _ready():
-	pos_inicial = $".".position.x
-	pos_esquerda = pos_inicial - dist_esquerda
-	pos_direita = pos_inicial + dist_direita
+	if orientacao:
+		pos_inicial = $".".position.x
+	else:
+		pos_inicial = $".".position.y
+	pos_1 = pos_inicial + dist_1	
+	pos_2 = pos_inicial - dist_2
 
 func _process(delta):
 	if direcao == 0:
@@ -23,12 +28,16 @@ func _process(delta):
 		else:
 			direcao = 2
 	if direcao == 1:
-		if $".".position.x <= pos_direita:
+		if $".".position.x <= pos_1 && orientacao:
 			$".".position.x += velocidade
+		elif $".".position.y <= pos_1 && !orientacao:
+			$".".position.y += velocidade
 		else:
 			direcao = 2
 	if direcao == 2:
-		if $".".position.x >= pos_esquerda:
+		if $".".position.x >= pos_2 && orientacao:
 			$".".position.x -= velocidade
+		elif $".".position.y >= pos_2 && !orientacao:
+			$".".position.y -= velocidade
 		else:
 			direcao = 1
